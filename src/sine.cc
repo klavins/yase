113b9adb9d38d1f3a65859b646d723ccdaa40071
Eric Klavins <klavins@uw.edu>
2021-12-06 15:16:46 -0800

    back to a sine wave

--- src/sine.cc
+++ src/sine.cc
@@ -5,10 +5,7 @@
 namespace yase {
 
   void Sine::update() {
-      outputs[signal] = sin(1 * 2 * M_PI * inputs[frequency] * n * TS) 
-                      + sin(2 * 2 * M_PI * inputs[frequency] * n * TS) / 2
-                      + sin(3 * 2 * M_PI * inputs[frequency] * n * TS) / 3
-                      + sin(4 * 2 * M_PI * inputs[frequency] * n * TS) / 4;
+      outputs[signal] = sin(1 * 2 * M_PI * inputs[frequency] * n * TS);
       n++;
   }    
 
