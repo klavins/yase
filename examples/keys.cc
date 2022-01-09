a2bd1bf3f0bef6c355381d06d87daabd19ead10b
Eric Klavins <klavins@uw.edu>
2022-01-08 21:25:11 -0800

    clean up

--- examples/keys.cc
+++ examples/keys.cc
@@ -6,7 +6,6 @@ using namespace yase;
 int main(int argc, char * argv[]) {
 
     Synthesizer synth("MIDI Mix");     
-
     Saw osc;
     Sine lfo;
     Audio audio;
@@ -15,40 +14,24 @@ int main(int argc, char * argv[]) {
     Biquad filter;
     Gain gain;
 
-    filter.set_type("lpf");
-
-    Fader cutoff(1000,6000), res(0.1,20), volume(0,1),
-          attack(0.005, 1),
-          decay(0.005, 1),
-          sustain(0,1),
-          release(0.005,1);
-    
+    filter.set_type("lpf");   
     osc.set_type("additive");
 
-    synth.add(osc)     .add(audio)     .add(midi)
-         .add(env)     .add(filter)    .add(cutoff)
-         .add(res)     .add(volume)    .add(gain)
-         .add(attack)  .add(decay)     .add(sustain)
-         .add(release);
+    synth.add(osc)     
+         .add(audio)
+         .add(midi)
+         .add(env)
+         .add(filter)
+         .add(lfo)
+         .add(gain);
 
-    // Main connections
     synth.connect( osc,    "signal", filter, "signal")
          .connect( filter, "signal", env,    "signal")
          .connect( env,    "signal", gain,   "signal")
          .connect( gain,   "signal", audio,  "left")
          .connect( gain,   "signal", audio,  "right");
 
-     // Fader connections
-     synth.connect( cutoff, "value",  filter, "frequency")
-          .connect( res,    "value",  filter, "resonance")
-          .connect( volume, "value",  gain,   "amplitude")
-          .connect( attack, "value",  env,    "attack")
-          .connect( decay,  "value",  env,    "decay")
-          .connect( sustain, "value", env,    "sustain")
-          .connect( release, "value", env,    "release");
-
-    // Midi Keyboard control
-    int num_keys = 0;
+    std::vector<int> keys;
     int keyboard_port = midi.get_port_id("Arturia KeyStep 32");
     synth.listen(MIDI_KEYDOWN, [&] (const Event &e) {
           if ( e.port == keyboard_port ) {
@@ -57,28 +40,30 @@ int main(int argc, char * argv[]) {
                filter.recalculate(); // TODO: This is awkward and could be forgotten
                env.set_input("velocity", e.value / 127.0);
                env.trigger();
-               num_keys++;
+               keys.push_back(e.id);
           }
      })
      .listen(MIDI_KEYUP, [&] (const Event &e) {
           if ( e.port == keyboard_port ) {
-               num_keys--;
-               if ( num_keys == 0 ) {
+               keys.erase(std::remove(keys.begin(), keys.end(), e.id), keys.end());
+               if ( keys.size() == 0 ) {
                     env.release();
+               } else {
+                    Event temp(MIDI_KEYDOWN, keys.back(), 0, 0);
+                    osc.set_input("frequency", temp.frequency());
+                    filter.set_input("offset", temp.frequency());
+                    filter.recalculate(); 
                }
           }
      });
 
-     // Fader - Midi mappings
-     synth.control(cutoff, 56)
-          .control(res, 60)
-          .control(volume, 62)
-          .control(attack, 19)
-          .control(decay, 23)
-          .control(sustain, 27)
-          .control(release, 31);
-
-     // MIDI Buttons
+     synth.control(env, "attack",  0.005, 1, 19)
+          .control(env, "decay",   0.005, 1, 23)
+          .control(env, "sustain", 0,     1, 27)
+          .control(env, "release", 0.005, 1, 31)
+          .control(gain, "amplitude", 0, 0.25, 62)
+          .control(filter, "frequency", 1000, 6000, 56)
+          .control(filter, "resonance", 0.1, 20, 60);
 
      synth.mutex({1, 4, 7}, {
           [&] (const Event &e) { osc.set_type("raw");      },
@@ -95,7 +80,6 @@ int main(int argc, char * argv[]) {
      synth.toggle(22, [&] (const Event &e) { filter.toggle(); }, true);
 
      synth.run(UNTIL_INTERRUPTED);
-
      synth.clear_leds();
 
      return 0;
--- examples/monophonic.cc
+++ examples/monophonic.cc
@@ -11,35 +11,39 @@ using namespace nlohmann;
 
 int main(int argc, char * argv[]) {
 
-    // Components
-    Synthesizer synth("MIDI Mix"); 
-    Audio audio;
-    Midi midi;
-    
-    std::ifstream config_stream("config/akai-monophonic.json");
-    json config, midi_map;
-    config_stream >> config; 
-    midi_map = config["midi_ids"];
-    int keyboard_port = midi.get_port_id("Arturia KeyStep 32");
-    int controller_port = midi.get_port_id("MIDI Mix");
-    string button_device_name = "MIDI Mix";
+     // Components
+     Synthesizer synth("MIDI Mix"); 
+     Audio audio;
+     Midi midi;
 
-    Mono mono(midi, midi_map, "MIDI Mix", keyboard_port, controller_port);
+     // Load config file
+     std::ifstream config_stream("config/akai-monophonic.json");
+     json config, midi_map;
+     config_stream >> config; 
+     midi_map = config["midi_ids"];
+     int keyboard_port = midi.get_port_id("Arturia KeyStep 32");
+     int controller_port = midi.get_port_id("MIDI Mix");
+     string button_device_name = "MIDI Mix";
 
-    synth.add(mono).add(audio).add(midi);
+     Mono mono(midi, midi_map, "MIDI Mix", keyboard_port, controller_port);
 
-    synth.connect(mono, "signal", audio, "left")
-         .connect(mono, "signal", audio, "right");
+     synth.add(mono)
+          .add(audio)
+          .add(midi);
 
-    synth.listen(MIDI_ANY, [&] (const Event &e) {
-         mono.inject(e); 
-    });   
+     synth.connect(mono, "signal", audio, "left")
+          .connect(mono, "signal", audio, "right");
 
-    // Go!
-    synth.run(UNTIL_INTERRUPTED);
+     synth.listen(MIDI_ANY, [&] (const Event &e) {
+          mono.inject(e); 
+     });   
+
+     // Go!
+     synth.run(UNTIL_INTERRUPTED);
 
      // Shutdown
-    synth.clear_leds();
-    return 0;
+     synth.clear_leds();
+     mono.clear_leds();
+     return 0;
 
 }
