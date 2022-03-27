10bd4d1f5569e3c224b33fcb5e932dcbd4321562
Eric Klavins <klavins@uw.edu>
2022-03-27 15:06:14 -0700

    slightly more compact Karplus-Strong

--- examples/KarplusStrong/karplus_strong.cc
+++ examples/KarplusStrong/karplus_strong.cc
@@ -4,29 +4,24 @@
 
 using namespace yase;
 
-#define LENGTH (((int) (SAMPLE_RATE/220)) - 3)
+#define LENGTH (((int) (SAMPLE_RATE/220)) - 1)
 
 class String : public Container {
 
 public:
   
-  String() : delay(LENGTH), prev(1), mixer(2) {
+  String() : delay(LENGTH), filter(1, 2) {
 
     signal = add_output("signal"); 
 
     add(delay);
-    add(prev);
-    add(mixer);
+    add(filter);
 
-    mixer.set_amplitude_input(0, 0.495);
-    mixer.set_amplitude_input(1, 0.495);
+    filter.set_coefficients({2.01},{1,1});
 
+    connect(delay, "signal", filter, "signal");
+    connect(filter, "signal", delay, "signal");
     equate_output("signal", delay, "signal");
-
-    connect(delay, "signal", mixer, 0);
-    connect(delay, "signal", prev, "signal");
-    connect(prev, "signal", mixer, 1);
-    connect(mixer, "signal", delay, "signal");
 
     std::default_random_engine generator;
     std::normal_distribution<double> distribution(0.0,1.0);
@@ -37,27 +32,13 @@ public:
         delay.update();
     }
 
-    // for(int i=0; i<LENGTH; i++) {
-    //     double y = (i>=10 && i <=20 ? 1: 0);
-    //     delay.set_input("signal", y);
-    //     delay.update();
-    // }    
-
-  }
-
-  void init() {
-    Container::init();
-  }
-
-  void update() {
-    Container::update();
   }
 
 private:
 
   int signal;
-  Delay delay, prev;
-  Mixer mixer;
+  Delay delay;
+  IIRFilter filter;
 
 };
 
@@ -68,7 +49,7 @@ int main(int argc, char * argv[]) {
     Envelope env;    
     Container synth;
 
-    env.set_input("attack", 0.375);
+    env.set_input("attack", 0.25);
     env.set_input("decay", 0.5);
     env.set_input("sustain", 1.0);
 
--- src/iir_filter.cc
+++ src/iir_filter.cc
@@ -1,20 +1,20 @@
 #include "module.hh"
 #include "iir_filter.hh"
-
+#include <iostream>
 
 namespace yase {
 
   IIRFilter::IIRFilter(int P, int Q) : p(P), q(Q) {
 
     for(int i=0; i<p; i++) {
-      u.push_back(0);
+      y.push_back(0);
       a.push_back(0);
     }
 
-    for(int i=0; i<p; i++) {
-      y.push_back(0);
+    for(int i=0; i<q; i++) {
+      u.push_back(0);
       b.push_back(0);
-    }
+    }    
 
     signal = add_input("signal");
     signal = add_output("signal"); 
@@ -30,15 +30,18 @@ namespace yase {
 
   void IIRFilter::update() {
 
+
     double s1 = 0, s2 = 0;
     int i;
 
-    for ( i=p-1; i >= 1; i-- ) u[i] = u[i-1];
-    for ( i=q-1; i >= 1; i-- ) y[i] = y[i-1];
-    for ( i=0; i<p; i++) s1 += b[i] * u[i];
-    for ( i=1; i<q; i++) s2 += a[i] * y[i];
-
+    for ( i=q-1; i >= 1; i-- ) u[i] = u[i-1];
     u[0] = inputs[signal];
+
+    for ( i=p-1; i >= 1; i-- ) y[i] = y[i-1];
+
+    for ( i=0; i<q; i++) s1 += b[i] * u[i];
+    for ( i=1; i<p; i++) s2 += a[i] * y[i];    
+
     y[0] = (s1-s2)/a[0];
 
     outputs[signal] = y[0];
