2f0b77d8cf6f96e2171dd1c5f88b463be025be71
Eric Klavins <klavins@uw.edu>
2022-05-30 08:18:23 -0700

    documentation for controls class

--- include/controls.hh
+++ include/controls.hh
@@ -5,6 +5,52 @@
 
 namespace yase {
 
+    //! Use a Controls object to connect MIDI knobs and faders to Module inputs. 
+    //! For example, to add a volume knob to your project, you can do 
+    //! 
+    //! \code
+    //! Container synth;
+    //! Gain gain;
+    //! Controls controls;
+    //! MidiInput midi("Your Device");
+    //! 
+    //! synth.add(gain)
+    //!      .add(conrols)
+    //!      .add(midi)
+    //!      .propagate_to(controls); // Important!
+    //!
+    //!               // module, input name,  min, max, midi id                  
+    //! controls.control(gain,   "amplitude", 0,   1,   123);
+    //! \endcode
+    //! 
+    //! Note that a controls object is a Container, just like the synth Container above. Thus,
+    //! it can receive events from the MidiInput object. However, you must explicitly tell the
+    //! synth container to propagate events to the controls container. 
+    //! 
+    //! Using the example above, you can now turn the knob on your controller with MIDI id
+    //! 123 and modulate the amlitude of the gain module. The controls object maps the MIDI
+    //! value, which ranges from 0 to 127, to the interval [0,1]. By default, this mapping
+    //! is linear. But you can make it exponential if you want more sensititvy at the low end. 
+    //! For example, 
+    //!
+    //! \code
+    //! controls.control(gain, "amplitude", 0, 1, 123)
+    //!         .exponential(1e4);
+    //! \endcode
+    //!
+    //! Here, the mapping between the MIDI knob position and the amplitude will follow
+    //! 
+    //! \f$\frac{(y-x)b^{m/127-1}}{b-1}+x\f$
+    //! 
+    //! where x and y are the minimum and maximum value of the control, b is the base of the
+    //! exponential (1e4 in this example), and m is the MIDI value. 
+    //! 
+    //! This is easy to see graphically. In the image below, the control output ranges from 2.0 to 5.0
+    //! and the response to the MIDI input is shown for various bases. 
+    //! 
+    //! \image html exponential-controls.png
+    //! 
+
     class Controls : public Container {
 
     public:
@@ -21,7 +67,7 @@ namespace yase {
       Controls &set_tracking_gain(double x);
       Controls &linear();
       Controls &exponential(double base);
-      
+
       void randomize_faders(); 
 
     private:
